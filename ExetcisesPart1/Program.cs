86e4aafc8a41b9d47c597f7b20ed4bd5dd516637
Opilane TTHK <opilane@tthk.ee>
2021-12-07 13:36:26 +0200

    add files

---
+++ ExetcisesPart1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ExetcisesPart1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            string fName, lName;
+            int Year, age;
+            Console.WriteLine("Enter your first name");
+            fName = Console.ReadLine();
+            Console.WriteLine("Enter your last name");
+            lName = Console.ReadLine();
+            Console.WriteLine($"Hello, {fName} {lName}");
+            Console.WriteLine("Enter the year of birth");
+            Year = Convert.ToInt32(Console.ReadLine());
+
+            int currentDate =DateTime.Now.Year;
+            age = currentDate - Year;
+
+            if (age >= 13)
+            {
+                Console.WriteLine("Welcome to Instagramm");
+            } else
+            {
+                Console.WriteLine("You ar etoo young to have an Instagramm account");
+             }
+        }
+
+    }
+}
---
+++ PersonalityTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PersonalityTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string color;
+            Console.WriteLine("Enter your favorite color");
+            color = Console.ReadLine().ToLower();
+
+            string template = $"You picked {color}";
+            if (color == "red")
+            {
+                Console.WriteLine($"{template} you are romantic");
+
+            }
+            if (color == "blue")
+            {
+                Console.WriteLine($"{template} you are reliable");
+            }
+            if (color == "yellow")
+            {
+                Console.WriteLine($"{template} you are cheerfull");
+            }
+            else
+            {
+                Console.WriteLine($"{template}. You are {color} unicorn");
+}
